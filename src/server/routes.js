9eea1ce5937e1f836d98bf8cc60229cc245a2dec
root <root@LAPTOP-96ECNO07.localdomain>
2024-05-22 14:27:37 +0700

    initial commit

---
+++ src/server/routes.js
@@ -0,0 +1,19 @@
+const postPredictHandler = require("../server/handler");
+
+const routes = [
+  {
+    path: "/predict",
+    method: "POST",
+    handler: postPredictHandler,
+    options: {
+      payload: {
+        maxBytes: 1000000,
+        /*Mengizinkan data berupa gambar*/
+        allow: "multipart/form-data",
+        multipart: true,
+      },
+    },
+  },
+];
+
+module.exports = routes;
